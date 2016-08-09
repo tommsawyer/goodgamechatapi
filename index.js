8ddd1c1c05b2cfaed35d673b39861a07bd9faf2f
Nikita Matrosov <matrosov.nikita94@gmail.com>
2016-08-09 21:47:04 +0300

    add get users method
    Note: distinction with GoodGame API

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ var GoodGameChat = require('./lib/GoodGame.js');
 
 var chat = new GoodGameChat();
 
-chat.connect().then(function() {
+/*chat.on('connection_send_msg', function(msg) {
+    console.log('connection_send_msg');
+    console.log(msg);
+});
+
+chat.on('connection_got_msg', function(msg) {
+    console.log('connection_got_msg');
+    console.log(msg);
+});*/
+chat.connect().then(function() {  
     chat.getChannels(0, 50).then(function(channels) {
         chat.joinChannel(channels[3]).then(function(channel) {
             console.log('Joined to: ' + channel.name);
-
-            channel.on('message', function(message) {
-                console.log(message.format('%t %n: %m'));
-            });
-
-        })
+            console.log("Users in channel " + channel.id);
+            chat.getUsersInChannel(channel.id).then(function(users) {
+                console.log(users);
+            });       
+        });
     });
-})
+});
