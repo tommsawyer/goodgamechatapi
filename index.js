2ec89c3421bbbf2477e2a6fb97b4df025bb28e67
Tom Sawyer <mortraineymusic@gmail.com>
2016-08-10 18:55:44 +0300

    added requests queue and errors handling

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ var GoodGameChat = require('./lib/GoodGame.js');
 
 var chat = new GoodGameChat();
 
-/*chat.on('connection_send_msg', function(msg) {
-    console.log('connection_send_msg');
-    console.log(msg);
-});
+// chat.on('connection_send_msg', function(msg) {
+//     console.log('connection_send_msg');
+//     console.log(msg);
+// });
 
-chat.on('connection_got_msg', function(msg) {
-    console.log('connection_got_msg');
-    console.log(msg);
-});*/
+// chat.on('connection_got_msg', function(msg) {
+//     console.log('connection_got_msg');
+//     console.log(msg);
+// });
+
 chat.connect().then(function() {  
     chat.getChannels(0, 50).then(function(channels) {
-        chat.joinChannel(channels[3]).then(function(channel) {
-            console.log('Joined to: ' + channel.name);
-            console.log("Users in channel " + channel.id);
-            chat.getUsersInChannel(channel.id).then(function(users) {
-                console.log(users);
-            });       
-        });
-    });
+        console.log('resolved succefuly! channels: ');
+        return channels;
+    })
+    .then(function(channels) {
+        chat.joinChannel('some_uncorrect_channel_id').catch(function(error) {
+            console.log(error);
+        })
+
+        chat.joinChannel(123123124124124).catch(function(error) {
+            console.log(error);
+        })
+    })
 });
