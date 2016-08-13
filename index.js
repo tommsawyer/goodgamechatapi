a688ca53ddb8adbe2cacbc6a3a176c5ed7eda86c
Nikita Matrosov <matrosov.nikita94@gmail.com>
2016-08-13 17:03:09 +0300

    Made available to add user to ignore list

--- index.js
+++ index.js
@@ -2,28 +2,37 @@ var GoodGameChat = require('./lib/GoodGame.js');
 
 var chat = new GoodGameChat();
 
-// chat.on('connection_send_msg', function(msg) {
-//     console.log('connection_send_msg');
-//     console.log(msg);
-// });
+ chat.on('connection_send_msg', function(msg) {
+     console.log('connection_send_msg');
+     console.log(msg);
+ });
 
-// chat.on('connection_got_msg', function(msg) {
-//     console.log('connection_got_msg');
-//     console.log(msg);
-// });
+ chat.on('connection_got_msg', function(msg) {
+     console.log('connection_got_msg');
+     console.log(msg);
+ });
 
 chat.connect().then(function() {  
-    chat.getChannels(0, 50).then(function(channels) {
-        console.log('resolved succefuly! channels: ');
-        return channels;
-    })
-    .then(function(channels) {
-        chat.joinChannel('some_uncorrect_channel_id').catch(function(error) {
-            console.log(error);
-        })
-
-        chat.joinChannel(123123124124124).catch(function(error) {
-            console.log(error);
-        })
-    })
+    chat.authorize('','').then(function(user){
+        chat.getChannels(0, 5).then(function(channels) {
+            return channels;
+        }).then(function(channels) {
+            chat.joinChannel(channels[3]).then(function(channel) {
+                return channel;
+            }).then(function(channel) {
+                chat.getUsersInChannel(channel.id).then(function(users) {
+                    return users;
+                }).then(function(users) {
+                    chat.addToIgnoreList(users[0].id).then(function(ignoreList) {
+                        console.log("ignoreList");
+                        console.log(ignoreList);
+                        return ignoreList;
+                    }).catch(function(err) {
+                        console.log("ERROR");
+                        console.log(err);
+                    });      
+                });  
+            });
+        });            
+    });
 });
